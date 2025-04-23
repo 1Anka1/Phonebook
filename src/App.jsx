7d57e60ca5a899381f34ec1720b36deff8777197
1Anka1 <annakasjanenko02@gmail.com>
2025-04-23 20:38:30 +0200

    initiation project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import { Home, Login, Register, Phonebook } from './pages';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="phonebook" element={<Phonebook />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/pages/index.jsx
@@ -0,0 +1,6 @@
+import Home from './Home/Home';
+import Login from './Login/Login';
+import Phonebook from './Phonebook/Phonebook';
+import Register from './Register/Register';
+
+export { Home, Login, Phonebook, Register };
